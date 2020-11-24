13c6aa17d7552bb8ca4e22d2e2ed207f48ce706e
Rurelawati <watirurela@gmail.com>
2020-11-24 16:49:06 +0700

    program_4

---
+++ data praktikum 4.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+n = 0
+nm = []
+nim = []
+tugas = []
+uts = []
+uas = []
+akhir = []
+
+while True:
+    Nama = input("Nama      :")
+    nm.append(Nama)
+    Nim = input("NIM       :")
+    nim.append(Nim)
+    Tugas = input("Tugas      :")
+    tugas.append(Tugas)
+    Uts = input("UTS       :")
+    uts.append(Uts)
+    Uas = input("UAS       :")
+    uas.append(Uas)
+    Akhir = (int(Tugas)* .30) + (int(Uts)* .35) + (int(Uas)* .35)
+    akhir.append(Akhir)
+    
+    data = ' '
+    while data != 'Y' and data != 'T':
+        data = input("Tambah data [Y/T] ?")
+        
+    n += 1
+    if data == 'T':
+        break
+    
+print("=================================================================")
+print("NO   |   NAMA   |   NIM    |  TUGAS   | UTS   |  UAS  |   AKHIR ")
+print("=================================================================")
+    
+for a in range(n):
+    print("",a+1, "|" ,nm[a], "|" , nim[a], "|", tugas[a], "|", uts[a], "|", uas[a], "|", akhir[a], "|")
+    
+    
+        
+    
+
